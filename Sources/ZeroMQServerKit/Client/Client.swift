5af42e111b5ea2e460478bed3becfeb64f026c40
Thiago Holanda <unnamedd@gmail.com>
2018-03-18 13:27:44 +0100

    implemented send methods to Client.swift

--- Sources/ZeroMQServerKit/Client/Client.swift
+++ Sources/ZeroMQServerKit/Client/Client.swift
@@ -5,6 +5,7 @@ public struct Client {
     private let context: Context
     private let config: SocketTuple
     
+    // MARK: - Initialization
     public init?(_ config: SocketProtocol) throws {
         guard let context = try? Context() else {
             fatalError("Context could not be instantiated")
@@ -22,10 +23,12 @@ public struct Client {
         self.config = (socket, url, config.type)
     }
     
+    // MARK: - Connection
     public func connect() throws {
         try self.config.socket.connect(self.config.url)
     }
     
+    // MARK: - Receiving Messages
     public func receive<T: Codable>(_ completion: (T) -> Void) throws {
         while let data: Data = try self.config.socket.receive() {
             let message = try JSONDecoder().decode(T.self, from: data)
@@ -44,5 +47,21 @@ public struct Client {
             completion(value)
         }
     }
+ 
+    // MARK: - Sending Messages
+    public func send<T: Encodable>(_ object: T) throws -> Bool {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(object)
+        let value = String(data: data, encoding: .utf8)
+        
+        return try send(value)
+    }
     
+    public func send(_ value: String) throws -> Bool {
+        return try config.socket.send(value)
+    }
+    
+    public func send(_ data: Data) throws -> Bool {
+        return try config.socket.send(data)
+    }
 }
